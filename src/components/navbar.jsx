5cb2742e7ab36c6c1f69a88d00ca87636967e024
AdryanOvie2 <adryanovie2@gmail.com>
2023-07-11 01:05:47 +0200

    header nav icon and dropdown

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,27 @@
 import '../styles/navbar.css'
+import 'boxicons'
 
 function Navbar (){
     return(
         <ul className='navbar'>
-            <li>Product</li>
-            <li>Templates</li>
+            <li className='dropbtn'>
+                Product
+                <box-icon className='icon-down-arrow'size='xs' name='down-arrow' type='solid' color='#000' ></box-icon>
+                <div className="dropdown-content">
+                    <a href="#">Product 1</a>
+                    <a href="#">Product 2</a>
+                    <a href="#">Product 3</a>
+                </div>
+            </li>
+            <li className='dropbtn'>
+                <p>Templates</p>
+                <box-icon className='icon-down-arrow'size='xs' name='down-arrow' type='solid' color='#000' ></box-icon>
+                <div className="dropdown-content">
+                    <a href="#">Template 1</a>
+                    <a href="#">Template 2</a>
+                    <a href="#">Template 3</a>
+                </div>
+            </li>
             <li>Pricing</li>
             <li>Reviews</li>
         </ul>
