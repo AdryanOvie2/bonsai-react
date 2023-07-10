e868573c101dd1150a8263405cd242364d86f327
AdryanOvie2 <adryanovie2@gmail.com>
2023-07-11 01:10:52 +0200

    header nav icon and dropdown

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar (){
         <ul className='navbar'>
             <li className='dropbtn'>
                 Product
-                <box-icon className='icon-down-arrow'size='xs' name='down-arrow' type='solid' color='#000' ></box-icon>
+                <box-icon className='icon-down-arrow'size='xs' name='chevron-down' type='solid' color='#000' ></box-icon>
                 <div className="dropdown-content">
                     <a href="#">Product 1</a>
                     <a href="#">Product 2</a>
@@ -15,7 +15,7 @@ function Navbar (){
             </li>
             <li className='dropbtn'>
                 <p>Templates</p>
-                <box-icon className='icon-down-arrow'size='xs' name='down-arrow' type='solid' color='#000' ></box-icon>
+                <box-icon className='icon-down-arrow'size='xs' name='chevron-down' type='solid' color='#000' ></box-icon>
                 <div className="dropdown-content">
                     <a href="#">Template 1</a>
                     <a href="#">Template 2</a>
